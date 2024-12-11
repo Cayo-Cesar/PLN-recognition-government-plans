49bb935a7f050471134934fbcfd75f3ff3cc5c30
Cayo Cesar <cayocesar652@gmail.com>
2024-12-11 15:09:09 -0300

    Dataset Saúde

--- preprocess/txt_to_csv.py
+++ preprocess/txt_to_csv.py
@@ -23,15 +23,15 @@ def txt_para_csv(caminho_txt, caminho_csv, cabecalho="Proposta"):
 
 # Converter os arquivos de treino para csv
 txt_para_csv("treino/saude.txt", "treino/saude.csv", "Saude")
-# txt_para_csv("preprocess/educacao.txt", "educacao.csv", "Educacao")
-# txt_para_csv("preprocess/seguranca.txt", "seguranca.csv", "Seguranca")
-# txt_para_csv("preprocess/meio_ambiente.txt", "meio_ambiente.csv", "Meio Ambiente")
-# txt_para_csv("preprocess/cultura.txt", "cultura.csv", "Cultura")
+# txt_para_csv(treino/educacao.txt", "treino/educacao.csv", "Educacao")
+# txt_para_csv(treino/seguranca.txt", "treino/seguranca.csv", "Seguranca")
+# txt_para_csv(treino/meio_ambiente.txt", "treino/meio_ambiente.csv", "Meio Ambiente")
+# txt_para_csv(treino/cultura.txt", "treino/cultura.csv", "Cultura")
 
 # Converter os arquivos de teste para csv
-#txt_para_csv("preprocess/saude_teste.txt", "saude_teste.csv", "Saude")
-# txt_para_csv("preprocess/educacao_teste.txt", "educacao_teste.csv", "Educacao")
-# txt_para_csv("preprocess/seguranca_teste.txt", "seguranca_teste.csv", "Seguranca")
-# txt_para_csv("preprocess/meio_ambiente_teste.txt", "meio_ambiente_teste.csv", "Meio Ambiente")
-# txt_para_csv("preprocess/cultura_teste.txt", "cultura_teste.csv", "Cultura")
+txt_para_csv("teste/saude_teste.txt", "teste/saude_teste.csv", "Saude")
+# txt_para_csv(teste/educacao_teste.txt", "teste/educacao_teste.csv", "Educacao")
+# txt_para_csv(teste/seguranca_teste.txt", "teste/seguranca_teste.csv", "Seguranca")
+# txt_para_csv(teste/meio_ambiente_teste.txt", "teste/meio_ambiente_teste.csv", "Meio Ambiente")
+# txt_para_csv(teste/cultura_teste.txt", "teste/cultura_teste.csv", "Cultura")
 
