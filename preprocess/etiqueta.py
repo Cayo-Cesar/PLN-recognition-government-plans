1f0f1bba3b5fbd8f02c11f4257ab4205e8ad6d90
Cayo Cesar <cayocesar652@gmail.com>
2024-12-17 15:49:45 -0300

    Adição Propostas Meio Ambiente

--- preprocess/etiqueta.py
+++ preprocess/etiqueta.py
@@ -3,18 +3,21 @@ import pandas as pd
 saude_df = pd.read_csv('dataset/saude.csv')
 seguranca_df = pd.read_csv('dataset/seguranca.csv')
 educacao_df = pd.read_csv('dataset/educacao.csv')
+meio_ambiente_df = pd.read_csv('dataset/meio_ambiente.csv')
 
 saude_df['Eixo'] = 'Saúde'
 seguranca_df['Eixo'] = 'Segurança'
 educacao_df['Eixo'] = 'Educação'
+meio_ambiente_df['Eixo'] = 'Meio Ambiente'
 
 saude_df = saude_df[['Proposta', 'Eixo']]
 seguranca_df = seguranca_df[['Proposta', 'Eixo']]
 educacao_df = educacao_df[['Proposta', 'Eixo']]
+meio_ambiente_df = meio_ambiente_df[['Proposta', 'Eixo']]
 
-df_completo = pd.concat([saude_df, seguranca_df, educacao_df], ignore_index=True)
+df_completo = pd.concat([saude_df, seguranca_df, educacao_df, meio_ambiente_df], ignore_index=True)
 
 print(df_completo.head())
 
-df_completo.to_csv('propostas_treino.csv', index=False)
+df_completo.to_csv('dataset/propostas.csv', index=False)
 
