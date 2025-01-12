10448eb5a201b53622e734decbef23682c4a02ec
Cayo Cesar <cayocesar652@gmail.com>
2025-01-12 11:29:18 -0300

    Adiciona suporte para o dataframe de infraestrutura no processamento de dados

--- preprocess/etiqueta.py
+++ preprocess/etiqueta.py
@@ -4,18 +4,21 @@ saude_df = pd.read_csv('dataset/saude.csv')
 seguranca_df = pd.read_csv('dataset/seguranca.csv')
 educacao_df = pd.read_csv('dataset/educacao.csv')
 meio_ambiente_df = pd.read_csv('dataset/meio_ambiente.csv')
+infraestrutura_df = pd.read_csv('dataset/infraestrutura.csv')
 
 saude_df['Eixo'] = 'Saúde'
 seguranca_df['Eixo'] = 'Segurança'
 educacao_df['Eixo'] = 'Educação'
 meio_ambiente_df['Eixo'] = 'Meio Ambiente'
+infraestrutura_df['Eixo'] = 'Infraestrutura'
 
 saude_df = saude_df[['Proposta', 'Eixo']]
 seguranca_df = seguranca_df[['Proposta', 'Eixo']]
 educacao_df = educacao_df[['Proposta', 'Eixo']]
 meio_ambiente_df = meio_ambiente_df[['Proposta', 'Eixo']]
+infraestrutura_df = infraestrutura_df[['Proposta', 'Eixo']]
 
-df_completo = pd.concat([saude_df, seguranca_df, educacao_df, meio_ambiente_df], ignore_index=True)
+df_completo = pd.concat([saude_df, seguranca_df, educacao_df, meio_ambiente_df, infraestrutura_df], ignore_index=True)
 
 print(df_completo.head())
 
